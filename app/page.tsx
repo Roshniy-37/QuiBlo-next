d8d038f3a12da5cdace92a59dd241747df3d0698
roshni <roshni.23545@sscbs.du.ac.in>
2024-08-10 12:23:32 +0530

    added sections in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
+import Footer from "@/components/footer";
 import HomeHeader from "@/components/HomeHeader";
+import Intro from "@/components/Intro";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <section className="h-24 border-b">
-      <HomeHeader/>
+        <HomeHeader/>
+      </section>
+      <section className="h-[75vh] border-b">
+        <Intro/>
+      </section>
+      <section className="h-[75vh] border-b">
+
+      </section>
+      <section className="h-24 border-t">
+      <Footer/>
       </section>
     </main>
   );
